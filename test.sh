d587e8c9ae15b323b347eb8fa80a183d038650ab
Richard Vaughan <vaughan@sfu.ca>
2013-01-10 13:42:01 -0800

    fresh for 2013: simplified code considerably

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ask Bash's built in time command to print only the real time
+TIMEFORMAT=%R
+
+for P in 10 50 100 200 300 400 500 600 800 1000 1200
+ do  
+   # record the population size
+   echo -e -n "$P\t" >> foo
+
+   # record the run time (time needs a fancy stederr redirect from a subshell)
+   { time ./universe -q -p$P  -u200 -d -z0; } 2>> foo
+done 
+
+  /usr/local/bin/gnuplot --persist <<\EOF
+  set title "Run time vs. Population"
+  plot "foo" using 1:2 with linespoints, x**2 / 300000 + 0.1
+EOF
+
