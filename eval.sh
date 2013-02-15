59042f2e3d2c46b0e17676b62bc842ebd5aa694e
Richard Vaughan <vaughan@sfu.ca>
2013-02-14 16:21:51 -0800

    improving eval

--- eval.sh
+++ eval.sh
@@ -8,7 +8,7 @@ SLP=0
 
 rm eval1 eval2 eval3
 
-for P in 10 100 200 300 400 500 600 800 1000 1500 2000 2500 3000 3500 4000
+for P in 10 100 200 300 400 500 600 800 1000 2000 3000 4000
  do  
    # record the population size
    echo -e -n "$P\t" >> eval1
@@ -16,16 +16,15 @@ for P in 10 100 200 300 400 500 600 800 1000 1500 2000 2500 3000 3500 4000
    echo -e -n "$P\t" >> eval3
 
    # record the run time (time needs a fancy stederr redirect from a subshell)
-   { time ./universe -w$WIN -q -p$P  -u100 -d -z$SLP -s1.0; } 2>> eval1
-   { time ./universe -w$WIN -q -p$P  -u100 -d -z$SLP -s5.0; } 2>> eval2
-   { time ./universe -w$WIN -q -p$P  -u100 -d -z$SLP -s0.2; } 2>> eval3
+   { time ./universe -q -w$WIN -p$P  -u100 -d -z$SLP -s1.0; } 2>> eval1
+   { time ./universe -q -w$WIN -p$P  -u100 -d -z$SLP -s2.0; } 2>> eval2
+   { time ./universe -q -w$WIN -p$P  -u100 -d -z$SLP -s5.0; } 2>> eval3  
 done 
 
   /usr/local/bin/gnuplot --persist <<\EOF
   set title "Run time vs. Population"
-  plot "eval1" using 1:2 with linespoints, \
-       "eval2" using 1:2 with linespoints, \
-       "eval3" using 1:2 with linespoints, \
-       x**2 / 300000 + 0.1
+  plot "eval1" using 1:2 with linespoints title "Size 1", \
+       "eval2" using 1:2 with linespoints title "Size 2", \
+       "eval3" using 1:2 with linespoints title "Size 5"
 EOF
 
